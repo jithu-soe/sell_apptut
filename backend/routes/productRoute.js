eb597f4ce25f05ef49b8db9ccee15287d3d16df9
jithu-soe <97289934+jithu-soe@users.noreply.github.com>
2024-07-05 02:25:11 +0000

    routes are added

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { Product } from "../models/productModel.js";
+
+const router = express.Router();
+
+//CREATE NEW PRODUCT ROUTE
+router.post('/', async (request, response) => {
+    try {
+        if (
+            !request.body.name ||
+            !request.body.priceInCents ||
+            !request.body.image ||
+            !request.body.category
+        ) {
+            return response.status(400).send({
+                message: 'Required fields are missing'
+            });
+        }
+
+        const newProduct = {
+            name: request.body.name,
+            priceInCents: request.body.priceInCents,
+            description: request.body.description,
+            image: request.body.image,
+            category: request.body.category
+        };
+
+        const product = await Product.create(newProduct);
+
+        return response.status(201).send(product);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//GET PRODUCT ROUTE
+router.get('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const product = await Product.findById(id);
+
+        return response.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+export default router;
